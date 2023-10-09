a7616e83348ff4c5bf39ccfbfcd9dd3369820b6d
chaejm55 <chaejm55@gmail.com>
2023-10-09 14:32:53 +0900

    [Fix] 한국 표준시로 설정

--- src/main/java/com/gdsc/mbti/MbtiApplication.java
+++ src/main/java/com/gdsc/mbti/MbtiApplication.java
@@ -1,5 +1,6 @@
 package com.gdsc.mbti;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class MbtiApplication {
+    @PostConstruct
+    void init() {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MbtiApplication.class, args);
