c1e757e831dc7acc93b33f297d21be00e6a92ac6
chaejm55 <chaejm55@gmail.com>
2023-10-13 01:14:02 +0900

    [Feat] 기본적 오류 통지 기능 추가(추후 보완 필요)

---
+++ src/main/java/com/gdsc/mbti/config/WebServerCustomizer.java
@@ -0,0 +1,18 @@
+package com.gdsc.mbti.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/errorpage/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/errorpage/500");
+        ErrorPage errorPage400 = new ErrorPage(HttpStatus.BAD_REQUEST, "/errorpage/400");
+        factory.addErrorPages(errorPage404, errorPage500, errorPage400);
+    }
+}
